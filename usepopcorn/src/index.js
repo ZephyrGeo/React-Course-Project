b12e994152088723adca8bc94dc3013bff59b147
ZephyrGeo <1255356291@qq.com>
2024-07-07 08:52:59 -0400

    add starRating example instance

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating size={30} />
-    <StarRating maxRating={10} color="blue" size={30} /> */}
+    {/* <StarRating size={30} /> */}
+    {/* <StarRating maxRating={10} color="blue" size={30} /> */}
   </React.StrictMode>
 );
