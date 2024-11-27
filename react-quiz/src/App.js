1a2712b3f67ddb23a0123c738d4c39750913bbba
ZephyrGeo <1255356291@qq.com>
2024-11-27 10:03:06 +0900

    feat: use fake api

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,7 +1,39 @@
 import Header from "./Header";
 import Main from "./Main";
+import { useEffect, useReducer } from "react";
 
+const initalState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("ACTION UNKNOWN");
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initalState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="App">
       <Header />
--- react-quiz/src/DateCounter.js
+++ react-quiz/src/DateCounter.js
@@ -1,19 +1,19 @@
-import {useReducer} from "react";
+import { useReducer } from "react";
 
-const initialState = {count: 0, step: 1};
+const initialState = { count: 0, step: 1 };
 
 const reducer = function (state, action) {
   console.log(state, action);
 
   switch (action.type) {
     case "inc":
-      return {...state, count: state.count + state.step};
+      return { ...state, count: state.count + state.step };
     case "dec":
-      return {...state, count: state.count - state.step};
+      return { ...state, count: state.count - state.step };
     case "setCount":
-      return {...state, count: action.payload};
+      return { ...state, count: action.payload };
     case "setStep":
-      return {...state, step: action.payload};
+      return { ...state, step: action.payload };
     case "reset":
       return initialState;
     default:
@@ -23,29 +23,29 @@ const reducer = function (state, action) {
 
 function DateCounter() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const {count, step} = state;
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch({type: "dec"});
+    dispatch({ type: "dec" });
   };
   const inc = function () {
-    dispatch({type: "inc"});
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    dispatch({type: "setCount", payload: Number(e.target.value)});
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    dispatch({type: "setStep", payload: Number(e.target.value)});
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    dispatch({type: "reset"});
+    dispatch({ type: "reset" });
   };
 
   return (
