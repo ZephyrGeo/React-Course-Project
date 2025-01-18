8c67bf5d01abe0ec4a431dce0d5370e2ba2da20c
ZephyrGeo <1255356291@qq.com>
2025-01-19 03:26:34 +0900

    feat: new components

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import AppLayout from "./pages/AppLayout";
+import NotFound from "./pages/NotFound";
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
